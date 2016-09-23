444954c80911a003f77adccfd032974a934862cf
Vagrant User <vagrant@vagrant.dev>
2016-09-23 18:23:51 +0000

    Debug points added

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,18 +5,25 @@ set -xe
 
 if [ $TRAVIS_BRANCH == "master" ] ; then
 
+	echo "Clears git information"
+  	rm -rf .git
+
     # setup ssh agent, git config and remote
+    git status # debug
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@104.236.118.124:/var/www/repotest"
+    git remote -v
+    git remote add deploy "ssh://travis@104.236.118.124://var/www/repotest"
     git config user.name "Travis CI"
     git config user.email "travis@test.com"
 
     # commit compressed files and push it to remote
+    git status # debug
     git add .
     git status # debug
+    git remote -v
     git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
-    git push -f deploy $TRAVIS_BRANCH:master
+    git push -f deploy master
 else
 
     echo "No deploy script for branch '$TRAVIS_BRANCH'"

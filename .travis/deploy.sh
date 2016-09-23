43fea5a205758552311d632dfa2587bb0eebc608
Vagrant User <vagrant@vagrant.dev>
2016-09-23 17:36:48 +0000

    Trying Travis

---
+++ .travis/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# print outputs and exit on first failure
+set -xe
+
+if [ $TRAVIS_BRANCH == "master" ] ; then
+
+    # setup ssh agent, git config and remote
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/travis_rsa
+    git remote add deploy "travis@104.236.118.124:/var/www/repotest"
+    git config user.name "Travis CI"
+    git config user.email "travis@test.com"
+
+    # commit compressed files and push it to remote
+    git add .
+    git status # debug
+    git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
+    git push -f deploy $TRAVIS_BRANCH:master
+else
+
+    echo "No deploy script for branch '$TRAVIS_BRANCH'"
+
+fi

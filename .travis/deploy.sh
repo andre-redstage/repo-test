bd620d0c462331bdcc736147ee9bb9fab3d41d41
Vagrant User <vagrant@vagrant.dev>
2016-09-23 19:36:01 +0000

    Try to push repo

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,22 +7,19 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
 
 	chmod 600 ~/.ssh/travis_rsa	
 
-    # setup ssh agent, git config and remote
-    git status # debug
+    # setup ssh agent, git config and remote    
     eval "$(ssh-agent -s)"
-
-    ssh-add ~/.ssh/travis_rsa
-    git remote -v
-    git remote add deploy "ssh://travis@104.236.118.124//var/www/repotest"
+    ssh-add ~/.ssh/travis_rsa        
+    git remote add deploy "travis@104.236.118.124:/var/www/repotest"
     git config user.name "Travis CI"
     git config user.email "travis@test.com"
 
-    # commit compressed files and push it to remote
-    git status # debug
+    # commit compressed files and push it to remote    
     git add .
-    git status # debug
-    git remote -v
+    git status # debug    
     git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
+
+    echo "Sends build"  	
     git push -f deploy master
 else
 

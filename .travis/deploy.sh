e1b88168cf0c23ecea6fda385b088fc205877a7b
Vagrant User <vagrant@vagrant.dev>
2016-10-10 20:36:39 +0000

    Test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,12 +4,17 @@
 set -xe
 
 if [ $TRAVIS_BRANCH == "master" ] ; then
-	
+    set -x	
     # setup ssh agent, git config and remote    
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/deploy-key        
-    chmod 600 ~/.ssh/deploy-key 
-    
+    openssl aes-256-cbc -K $encrypted_4dc565c56c51_key -iv $encrypted_4dc565c56c51_iv -in deploy-key.enc -out deploy-key -d
+    rm deploy-key.enc # Don't need it anymore
+    chmod 600 deploy-key
+    mv deploy-key ~/.ssh/id_rsa
+
+    # eval "$(ssh-agent -s)"
+    # ssh-add ~/.ssh/deploy-key        
+    # chmod 600 ~/.ssh/deploy-key 
+
     git remote add deploy "travis@104.236.118.124:/var/www/repotest"
     git config user.name "Travis CI"
     git config user.email "travis@test.com"
